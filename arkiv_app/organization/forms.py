5b39237109ab7052ee3ca775609b0669147d20ba
Bruno.R.Belo <102493231+BrunoRochaBelo@users.noreply.github.com>
2025-06-06 14:25:28 -0300

    Document user management screen

--- arkiv_app/organization/forms.py
+++ arkiv_app/organization/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.fields import EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class OrganizationForm(FlaskForm):
@@ -8,3 +9,18 @@ class OrganizationForm(FlaskForm):
     description = TextAreaField('Descrição')
     custom_domain = StringField('Domínio Personalizado')
     submit = SubmitField('Salvar')
+
+
+class InviteUserForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    role = SelectField(
+        'Papel',
+        choices=[
+            ('MANAGER', 'MANAGER'),
+            ('EDITOR', 'EDITOR'),
+            ('CONTRIBUTOR', 'CONTRIBUTOR'),
+            ('VIEWER', 'VIEWER'),
+        ],
+        validators=[DataRequired()],
+    )
+    submit = SubmitField('Convidar')
