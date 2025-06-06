a5b1798aae9b5697a98b278cf5a18500ce9bd5f2
Bruno.R.Belo <102493231+BrunoRochaBelo@users.noreply.github.com>
2025-06-06 14:07:56 -0300

    Add tag management screen with search and filters

--- arkiv_app/tag/forms.py
+++ arkiv_app/tag/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+from ..models import Tag
+from ..utils import current_org_id
 
 
 class TagForm(FlaskForm):
-    name = StringField('Nome', validators=[DataRequired()])
-    color_hex = StringField('Cor')
-    submit = SubmitField('Salvar')
+    """Formulário para criação/edição de tags."""
+
+    name = StringField("Nome", validators=[DataRequired()])
+    color_hex = StringField("Cor")
+    submit = SubmitField("Salvar")
+
+    def __init__(self, original=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Tag instance being edited, for uniqueness validation
+        self.original = original
+
+    def validate_name(self, field):
+        org_id = current_org_id()
+        if org_id is None:
+            return
+        query = Tag.query.filter_by(org_id=org_id, name=field.data)
+        if self.original is not None:
+            query = query.filter(Tag.id != self.original.id)
+        if query.first():
+            raise ValidationError("Nome de tag j\u00e1 existe")
